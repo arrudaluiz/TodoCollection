27a74f1cd6dbb566bb3034fbbdaa4c7969e2da39
Luiz Alberto Teodoro de Arruda <arrudaluiz89@gmail.com>
2019-06-02 02:32:03 -0300

    Implemented composeEmail function

--- app/src/main/java/br/edu/utfpr/todocollection/ActAbout.java
+++ app/src/main/java/br/edu/utfpr/todocollection/ActAbout.java
@@ -1,9 +1,12 @@
 package br.edu.utfpr.todocollection;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ActAbout extends AppCompatActivity {
     private Toolbar toolbar;
@@ -17,5 +20,14 @@ public class ActAbout extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void composeEmail(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse(getString(R.string.mailto))); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
